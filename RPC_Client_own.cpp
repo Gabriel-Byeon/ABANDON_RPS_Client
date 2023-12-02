e52dd0bf5e39104fdc73a2b9d3753e0741dde08c
byeon <bhc618@gmail.com>
2023-12-03 00:38:46 +0900

    게임을 시작할 때 진행 요청을 받고, 그 이후로는 아무떄나 종료 요청, 승률 요청을 받을 수 있다. (가독성 향상, 코드 통일, 멘트 추가)

--- RPC_Client_own.cpp
+++ RPC_Client_own.cpp
@@ -51,8 +51,10 @@ int main() {
     
     Packet packet;
 
-    std::cout << "묵찌빠 게임을 하시겠습니까?(예(1) / 아니오(0)) : ";
+    std::cout << "묵찌빠 게임 한판 어떠신가요? " << "낯선 상대가 게임을 요청했습니다! (도전에 응한다!(1) / 도망간다(0)) : ";
     std::cin >> start_request;
+    std::cout << std::endl << "도전에 응하셨군요! 건투를 빕니다!" << std::endl;
+
     packet.start_game_request = start_request;
     send(clientSocket, (char*)&packet, sizeof(Packet), 0);
 
@@ -62,7 +64,8 @@ int main() {
 
             int clientHand;
 
-            std::cout << std::endl << "가위 바위 보 게임을 시작합니다." << std::endl;
+            
+            std::cout << std::endl << "선공을 위한 가위 바위 보 게임을 시작합니다!!" << std::endl;
             std::cout << "바위(0), 가위(1), 보(2) 중 하나를 선택하세요(승률(3), 종료(4)): ";
             std::cin >> clientHand;
 
@@ -80,9 +83,12 @@ int main() {
                 if (count > 0) {
                     
                     recv(clientSocket, (char*)&packet, sizeof(Packet), 0);
-                    std::cout << std::endl << "<<<<승률 출력>>>>" << std::endl;
-                    std::cout << "서버의 승률 : " << 1.0 - packet.winrate << std::endl;
-                    std::cout << "클라이언트의 승률 : " << packet.winrate << std::endl;
+                    std::cout << std::endl << "<<<<승률 정보 출력>>>>" << std::endl;
+                    std::cout << std::endl << "경기 수 : " << count << " 판" << std::endl;
+                    std::cout << std::endl << "서버가 이긴 경기 수 : " << packet.count - packet.win << " 판" << std::endl;
+                    std::cout << std::endl << "서버의 승률 : " << (1.0 - packet.winrate) * 100 << "%" << std::endl;
+                    std::cout << std::endl << "클라이언트가 이긴 경기 수 : " << packet.win << " 판" << std::endl;
+                    std::cout << std::endl << "클라이언트의 승률 : " << packet.winrate * 100 << "%" << std::endl;
                     continue;
                 }
                 std::cout << std::endl << "아직 한 판도 안하셨습니다." << std::endl;
@@ -91,7 +97,7 @@ int main() {
             
           
 
-            std::cout << "서버 선택한 것: ";
+            std::cout << "서버가 선택한 것: ";
             if (packet.choice_S == ROCK) {
                 std::cout << "바위      ";
             }
@@ -116,15 +122,15 @@ int main() {
             recv(clientSocket, (char*)&packet, sizeof(Packet), 0);
             
             if (packet.Att == 0) {
-                std::cout << "무승부입니다." << std::endl;
+                std::cout << "무승부입니다!! 재경기를 진행하세요!" << std::endl;
             }
             else if (packet.Att == 1) {
-                std::cout << "서버가 공격입니다." << std::endl;
+                std::cout << "아쉽군요! 서버가 공격입니다!" << std::endl;
                 packet.Game_Choose = 1;
                 break;
             }
             else if (packet.Att == -1) {
-                std::cout << "클라이언트가 공격입니다." << std::endl;
+                std::cout << "클라이언트가 공격입니다. 기회를 놓치지 마세요!!" << std::endl;
                 packet.Game_Choose = 1;
                 break;
             }
@@ -134,7 +140,7 @@ int main() {
 
             int clientChoice;
 
-            std::cout << std::endl << "묵찌빠 게임을 시작합니다." << std::endl;
+            std::cout << std::endl << "묵찌빠 게임을 시작합니다!!" << std::endl;
             std::cout << "바위(0), 가위(1), 보(2) 중 하나를 선택하세요(승률(3), 종료(4)): ";
             std::cin >> clientChoice;
             packet.choice_C = clientChoice;
@@ -148,17 +154,20 @@ int main() {
             else if (packet.choice_C == WIN_REQUEST) {
                 if (count > 0) {
                     recv(clientSocket, (char*)&packet, sizeof(Packet), 0);
-                    std::cout << std::endl << "<<<<승률 출력>>>>" << std::endl;
-                    std::cout << std::endl << "서버의 승률 : " << 1.0 - packet.winrate << std::endl;
-                    std::cout << std::endl << "클라이언트의 승률 : " << packet.winrate << std::endl;
+
+                    std::cout << std::endl << "<<<<승률 정보 출력>>>>" << std::endl;
+                    std::cout << std::endl << "경기 수 : " << count << " 판" << std::endl;
+                    std::cout << std::endl << "서버가 이긴 경기 수 : " << packet.count - packet.win << " 판" << std::endl;
+                    std::cout << std::endl << "서버의 승률 : " << (1.0 - packet.winrate) * 100 << "%" << std::endl;
+                    std::cout << std::endl << "클라이언트가 이긴 경기 수 : " << packet.win << " 판" << std::endl;
+                    std::cout << std::endl << "클라이언트의 승률 : " << packet.winrate * 100 << "%" << std::endl;
                     continue;
                 }
-                std::cout << std::endl << "아직 한 판도 안하셨습니다." << std::endl;
+                std::cout << std::endl << "한 판은 진행하시고 승률을 요청해주세요." << std::endl;
                 continue;
             }
             else {
 
-                recv(clientSocket, (char*)&packet, sizeof(Packet), 0);
                 send(clientSocket, (char*)&packet, sizeof(Packet), 0);
                 recv(clientSocket, (char*)&packet, sizeof(Packet), 0);
              
@@ -200,12 +209,17 @@ int main() {
         if (packet.end == 1) {
             if (count > 0) {
                 recv(clientSocket, (char*)&packet, sizeof(Packet), 0);
-                std::cout << std::endl << "<<<<<<<<최종 승률>>>>>>>>" << std::endl;
-                std::cout << "서버의 승률 : " << 1.0 - packet.winrate << std::endl;
-                std::cout << "클라이언트의 승률 : " << packet.winrate << std::endl;
+
+                std::cout << std::endl << "<<<<<<<<최종 승률 정보 출력>>>>>>>>" << std::endl;
+                std::cout << std::endl << "총 경기 수 : " << count << " 판" << std::endl;
+                std::cout << std::endl << "서버가 이긴 총 경기 수 : " << packet.count - packet.win << " 판" << std::endl;
+                std::cout << std::endl << "서버의 최종 승률 : " << (1.0 - packet.winrate) * 100 << "%" << std::endl;
+                std::cout << std::endl << "클라이언트가 이긴 총 경기 수 : " << packet.win << " 판" << std::endl;
+                std::cout << std::endl << "클라이언트의 최종 승률 : " << packet.winrate * 100 << "%" << std::endl;
+                break;
             }
             else
-                std::cout << std::endl << "한 판도 안하셔도 승률을 낼 수 없습니다." << std::endl;
+                std::cout << std::endl << "한 판은 진행하시고 승률을 요청해주세요." << std::endl;
             break;
         }
         else {
@@ -217,7 +231,7 @@ int main() {
 
         return 0;
     }
-    std::cout << std::endl << "프로그램을 종료하겠습니다." << std::endl;
+    std::cout << std::endl << "프로그램을 종료하겠습니다.다음에 또 뵙겠습니다~" << std::endl;
     return 0;
     
 }
